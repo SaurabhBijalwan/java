1960520deb8e70760faff557f37d1a52cbbc40db
SaurabhBijalwanji <bijalwangf@gmail.com>
2020-10-21 12:56:45 +0530

    first commit

---
+++ Interview/Answer2P.java
@@ -0,0 +1,30 @@
+class NodeP {
+int data;
+NodeP next;
+
+NodeP(int d){
+data=d;
+next=null;
+
+}
+
+
+
+
+}
+
+
+
+class MergeListP{
+
+    
+}
+
+
+class Answer2P {
+    public static void main(String args[]){
+
+        System.out.println("Node");
+    }
+    
+    }
