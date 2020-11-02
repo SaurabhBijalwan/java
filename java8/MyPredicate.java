8977d4cc284d9469934f52f5c63c2f7eae075417
SaurabhBijalwanji <bijalwangf@gmail.com>
2020-11-02 12:57:52 +0530

    first commit

--- java8/MyPredicate.java
+++ java8/MyPredicate.java
@@ -5,8 +5,24 @@ public class MyPredicate {
     public static void main(String args[]){
 
         Predicate<Integer> p = i->i%2==0;
-        
+
         System.out.println(p.test(10)); //true
         System.out.println(p.test(11)); //false
+
+        TCSEmployee e1 = new TCSEmployee(100000,true);
+        Predicate<TCSEmployee> p1 =emp->emp.salary<10000 && emp.hasGF==true;
+        System.out.println(p1.test(e1));
     }
+}
+
+class TCSEmployee {
+ int salary ;
+boolean hasGF ;
+TCSEmployee(int salary ,boolean hasGF){
+    this.salary=salary;
+    this.hasGF=hasGF;
+}
+
+
+
 }
